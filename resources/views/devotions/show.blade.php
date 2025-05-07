460b9e686757491b761962a2b88fbf7f486de193
Keller Martin <kellerjmrtn@gmail.com>
2025-05-06 22:47:53 -0400

    Add Title Customization

--- resources/views/devotions/show.blade.php
+++ resources/views/devotions/show.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title'){{ $devotion->date->format('F j, Y') }} | {{ $devotion->title }}@endsection
+
 @section('navLeft')
     <div class="nav-date">
         {{ $devotion->date->format('F j, Y') }}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name') }}</title>
+        <title>@yield('title', config('app.name'))</title>
 
         <link rel="icon" type="image/png" href="/favicon.png">
 
@@ -21,9 +21,6 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Martel:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet">
         
-        {{-- Dev only for HTTP requests --}}
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
         @stack('stylesheets')
         
         <!-- Scripts -->
