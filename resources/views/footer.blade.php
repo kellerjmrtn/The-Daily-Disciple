651f925f59f1f87154aa4c2b9af63eeb3519f80e
Keller Martin <kellerjmrtn@gmail.com>
2025-05-06 23:03:59 -0400

    Update Copyright

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,7 +5,7 @@
             <i>And he said to all, “If anyone would come after me, let him deny himself and take up his cross daily and follow me. Luke 9:23 (ESV)</i>
         </div>
         <div class="footer-copyright">
-            ©2024
+            ©2024-{{ now()->format('Y') }}
         </div>
     </div>
 </div>
