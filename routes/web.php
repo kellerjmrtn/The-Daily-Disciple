888cb0ed725ba7a512cf6a5ccfccd1d2a47d11dc
Keller Martin <kellerjmrtn@gmail.com>
2025-05-10 12:36:45 -0400

    Add Health Check Endpoint

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@ Route::middleware('auth')->group(function () {
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/health', function () {
+    return response('', 200);
+});
+
 require __DIR__.'/auth.php';
